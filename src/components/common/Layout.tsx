1ab976a36ad87080c6e980ee8332e38d6d1188fe
Salvatore De Rosa <zoomsulweb@gmail.com>
2022-11-29 12:14:17 +1100

    installed packages, styled-comp - react-router

---
+++ src/components/common/Layout.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import styled from "styled-components";
+import { Footer } from "./Footer";
+import { Navbar } from "./Navbar";
+
+const LayoutStyle = styled.section.attrs({ className: "layout-container" })``;
+export const Layout = () => {
+  // Scroll To Top of Page at every Change of Page/Route
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <LayoutStyle>
+      <Navbar />
+      <Footer />
+    </LayoutStyle>
+  );
+};
