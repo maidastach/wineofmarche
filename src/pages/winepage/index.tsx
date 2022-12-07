0dccfc3fbe7f41014e6bc860949839a44a5db684
Salvatore De Rosa <zoomsulweb@gmail.com>
2022-12-07 15:46:13 +1100

    seo & structured data stage1

--- src/pages/winepage/index.tsx
+++ src/pages/winepage/index.tsx
@@ -6,6 +6,8 @@ import winelist from "../../data/data-wines.json";
 import { RelatedProducts } from "./RelatedProducts";
 import { WineCard } from "./WineCard";
 import wineBgPic from "../../assets/images/winebg.jpg";
+import { Helmet } from "react-helmet";
+import { imagesOfWines } from "../../data/data-wine-images";
 
 const WinePageStyle = styled.section`
   display: grid;
@@ -166,6 +168,83 @@ export const WinepageScreen = () => {
 
   return (
     <WinePageStyle className={`winepage-container ${wineId}`}>
+      <Helmet>
+        <title>
+          {wine.name} | {wine.type.split(",")[0]} | Wine of Marche
+        </title>
+
+        <meta name="googlebot" content="index,follow" />
+        <meta name="robots" content="index,follow" />
+
+        <link
+          rel="canonical"
+          href={`${process.env.REACT_APP_BASE_URL}/wines/${wine.id}`}
+        />
+
+        <meta
+          property="og:title"
+          content={`${wine.name} | ${wine.type.split(",")[0]} | Wine of Marche`}
+        />
+        <meta name="description" content={wine.type} />
+        <meta property="og:description" content={wine.type} />
+        <meta
+          property="og:url"
+          content={`${process.env.REACT_APP_BASE_URL}/wines/${wine.id}`}
+        />
+        <meta property="keywords" content={`${wine.id} ${wine.type}`} />
+        <meta property="og:type" content="Product" />
+        <meta
+          property="og:image"
+          content={`${process.env.REACT_APP_BASE_URL}${
+            imagesOfWines.find((w) => w.id === wineId)?.image
+          }`}
+        />
+        <meta property="og:image:alt" content={wine.name} />
+        <meta property="og:image:width" content="200" />
+        <meta property="og:image:height" content="400" />
+        <meta property="og:locale" content="en_EN" />
+        <meta property="og:site_name" content="Wine of Marche" />
+
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Product",
+            name: wine.name,
+            description: wine.type,
+            image: [
+              `${process.env.REACT_APP_BASE_URL}${
+                imagesOfWines.find((w) => w.id === wineId)?.image
+              }`,
+            ],
+            brand: {
+              "@type": "Organization",
+              name: "Wine of Marche",
+              "@id": "https://wineofmarche.au",
+            },
+            review: {
+              "@type": "Review",
+              reviewRating: {
+                "@type": "Rating",
+                ratingValue: "5",
+                bestRating: "5",
+                worstRating: "4",
+              },
+              author: { "@type": "Person", name: "Salvatore De Rosa" },
+            },
+            audience: {
+              "@type": "Audience",
+              name: "wine lovers",
+            },
+            category: "Alcohol, Wines",
+            countryOfAssembly: "Italy",
+            countryOfOrigin: {
+              address: "Italy",
+            },
+            keywords: wine.type,
+            url: `${process.env.REACT_APP_BASE_URL}/wines/${wine.id}`,
+          })}
+        </script>
+      </Helmet>
       <div className="winepage-content">
         <MaxWidthComponent label={wineId}>
           <WineCard wine={wine} />
